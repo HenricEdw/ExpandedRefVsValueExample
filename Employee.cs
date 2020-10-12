639712e5cbc7219110e8795503d6320f67b44c55
Henric Edwards <62654315+HenricEdw@users.noreply.github.com>
2020-10-12 12:26:06 +0200

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueVsRefTypes2
+{
+    enum Department
+    {
+        HR,
+        IT
+    }
+    class Employee
+    {
+        public string Name { get;  set; }
+        public Department Avdelning { get; set; }
+
+        public Employee(string namn, Department department)
+        {
+            Name = namn;
+            Avdelning = department;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueVsRefTypes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("Henric", Department.HR);
+            //för värdetyper kan saker bli null
+            //Jag vill hämta längden på namnet men har ännu inte gett namn ett värde
+            //Finns olika sätt att kontrollera mot null
+            
+            //emp.Name = "Henric";
+
+            EmployeeStruct empStruct = new EmployeeStruct();
+            empStruct.Name = "Bertil";
+            emp.Avdelning = Department.HR;
+            Employee newEmp = emp;
+
+            EmployeeStruct newEmpStruct = empStruct;
+
+            ChangeEmp(newEmp);
+
+            ChangeEmpStruct(newEmpStruct);
+
+            Console.Read();
+        }
+
+        private static void ChangeEmpStruct(EmployeeStruct empStr)
+        {
+            empStr.Name = "Torild";
+        }
+
+        private static void ChangeEmp(Employee empInstance)
+        {
+            empInstance.Name = "Berit";
+        }
+    }
+}
